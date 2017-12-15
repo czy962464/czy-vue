4498750987d1a1ade004d170f299be7760e61cb1
czy <czy521962464@162.com>
2017-12-15 22:36:15 +0800

    171215

--- src/page/index/index.vue
+++ src/page/index/index.vue
@@ -1,18 +1,80 @@
 <template>
   <div>
     <header class="header">
-      <div class="back">返回</div>
-      <div class="search"></div>
+      <div class="back iconfont">&#xe624;</div>
+      <div class="search">
+        <a href="#" class="prompt">输入城市/景点、游玩</a>
+      </div>
       <div class="city">城市</div>
     </header>
-
+    <swiper :options="swiperOption">
+      <swiper-slide v-for='item in swiperInfo' :key='item.id'>
+        <div class="swiper-img-con">
+          <img  class="swiper-img" :src='item.imgUrl'/>
+        </div>
+      </swiper-slide>
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+    <swiper :options="{}">
+      <swiper-slide v-for='(pageInfo, index) in pages' :key='index'>
+        <div class="icon-wrapper">
+          <div v-for='item in pageInfo' :key='item.id' class="icon-item">
+            <div class="icon-img-con">
+              <img  class="icon-img" :src='item.imgUrl'/>
+            </div>
+          </div>
+        </div>
+      </swiper-slide>
+    </swiper>
+    
   </div>
 </template>
 
 <script>
-export default {
-  name: 'Index'
-}
+  export default {
+    name: 'Index',
+    data () {
+      return {
+        swiperInfo: [],
+        iconInfo: [],
+        swiperOption: {
+          autoplay: 10000,
+          pagination: '.swiper-pagination',
+          loop: true
+        }
+      }
+    },
+    computed: {
+      pages () {
+        const pages = []
+        this.iconInfo.forEach((value, index) => {
+          let page = Math.floor(index / 8)
+          if (!pages[page]) {
+            pages[page] = []
+          }
+          pages[page].push(value)
+        })
+        return pages
+      }
+    },
+    methods: {
+      getIndexData () {
+        this.$http.get('/static/index.json')
+            .then(this.handelGetDataSucc.bind(this))
+      },
+      handelGetDataSucc (res) {
+        const body = res.body
+        if (body && body.data && body.data.swiper) {
+          this.swiperInfo = body.data.swiper
+          this.iconInfo = body.data.icons
+          console.log(this.iconInfo)
+        }
+      }
+    },
+    created () {
+      this.getIndexData()
+    }
+  }
 </script>
 
 <style scoped>
@@ -24,6 +86,7 @@ export default {
   .back {
     width: .64rem;
     line-height: .86rem;
+    text-align: center;
   }
   .search {
     flex: 1;
@@ -31,9 +94,51 @@ export default {
     background: #fff;
     border-radius: .1rem;
   }
+  .prompt {
+    font-size: 0.26rem;color: #ccc;line-height: 0.6rem;
+  }
   .city {
     width: 1.14rem;
     line-height: .86rem;
-    text-align: center;
+    text-align: left;
+    overflow: hidden;
+    white-space: nowrap;
+  }
+  .city::after{
+    content:"";
+    display: block;
+    width: 0px;
+    height: 0px;
+    border: 0.1rem solid #fff;
+    border-color: white transparent transparent transparent;
+    position: absolute;
+    right: 0.3rem;
+    top: 0.36rem;
+  }
+  .swiper-img-con {
+    overflow: hidden;
+    width: 100%;
+    height: 0;
+    padding-bottom: 31.25%;
+  }
+  .swiper-img {
+    width: 100%;
+  }
+  .icon-wrapper {
+    
+  }
+  .icon-item {
+    box-sizing: border-box;
+    float: left;
+    width: 25%;
+    padding: 0.4rem;
+  }
+  .icon-img-con {
+    width:100%;
+    height: 0;
+    padding-bottom: 100%;
+  }
+  .icon-img {
+    width: 100%;
   }
 </style>
